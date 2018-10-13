a59c1b7e43dfd7af48aea95e9c1bc1759a6e19ac
Matlink <matlink@matlink.fr>
2018-10-13 17:22:06 +0200

    Print SecurityCheckError details

--- gpapi/googleplay.py
+++ gpapi/googleplay.py
@@ -220,7 +220,7 @@ class GooglePlayAPI(object):
                 ac2dmToken = params["auth"]
             elif "error" in params:
                 if "NeedsBrowser" in params["error"]:
-                    raise SecurityCheckError()
+                    raise SecurityCheckError(params["error"])
                 raise LoginError("server says: " + params["error"])
             else:
                 raise LoginError("Auth token not found.")
